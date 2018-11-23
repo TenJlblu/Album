2a20f4b75e20f0c0a1a5481d62539728d979d847
TenJlblu <vasuautov@gmail.com>
2018-11-23 12:21:23 -0800

    Initial commit

---
+++ venv/RootClass.py
@@ -0,0 +1,38 @@
+import os
+import pygame
+import time
+from Album import Album
+from Item import Item
+SCREEN_RESOLUTION = (1000, 600)
+pygame.init()
+
+class Root:
+    Albums = []
+    Location = ""
+    Name = ""
+    Pointer = 0
+    Current_Album = None
+    Buffer = []
+
+    def __init__(self, location, name):
+        self.Name = name
+        self.Albums.append([])
+        self.Location = location + "\\" + name
+
+    def get_path_name(self):
+        return self.Location
+
+    def init(self, directory, current_album, go_deeper):
+        for item in os.listdir(directory):
+            if os.path.isdir(directory + "/" + item) and go_deeper:
+                new_album = Album(item, directory)
+                self.Albums[0].append(new_album)
+                self.init(directory + "/" + item, new_album, False)
+            else:
+                try:
+                    image = pygame.image.load(directory + "/" + item)
+                    #print(current_album.Name)
+                    current_album.add_item(Item(image, item, directory))
+                except Exception:
+                    print(9)
+                    continue
